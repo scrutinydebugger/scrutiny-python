f168c7c64b1cc7b4dee01a28373d44aff16dc128
Frederic Morin <frederic.morin.8@gmail.com>
2021-04-14 22:40:20 -0400

    [MAJOR] Inital firmware related code commit

---
+++ firmware/lib/src/scrutiny/scrutiny.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "scrutiny.hpp"
+
+void scrutiny_init() {
+    std::cout << "scrutiny_init" << std::endl;
+}
+
+void scrutiny_tick() {
+    std::cout << "scrutiny_tick" << std::endl;
+}
---
+++ firmware/lib/src/scrutiny/scrutiny.hpp
@@ -0,0 +1,2 @@
+void scrutiny_init();
+void scrutiny_tick();
