10eac60a9d473a3f261e6b98e8cba1044a6ea74e
Pier-Yves Lessard <py.lessard@gmail.com>
2021-09-30 16:59:42 -0400

    uniformalized gtest unit test to use class with setUp call

--- firmware/test/protocol/test_protocol_rx_parsing.cpp
+++ firmware/test/protocol/test_protocol_rx_parsing.cpp
@@ -3,63 +3,65 @@
 #include "scrutiny.h"
 #include "scrutiny_test.h"
 
-//=============================================================================
-TEST(TestProtocol_V1_0, TestRx_ZeroLen_AllInOne)
+class TestRxParsing : public ::testing::Test 
 {
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
+protected:
+   scrutiny::Timebase tb;
+   scrutiny::Protocol::CommHandler comm;
 
+   TestRxParsing() {}
+
+   virtual void SetUp() 
+   {
+      comm.init(1,0, &tb);
+   }
+};
+
+//=============================================================================
+TEST_F(TestRxParsing, TestRx_ZeroLen_AllInOne)
+{
   uint8_t data[8] = {1,2,0,0};
   scrutiny_test::add_crc(data, 4);
-  proto.process_data(data, sizeof(data));
+  comm.process_data(data, sizeof(data));
 
-  ASSERT_TRUE(proto.request_received());
-  scrutiny::Protocol::Request* req = proto.get_request();
+  ASSERT_TRUE(comm.request_received());
+  scrutiny::Protocol::Request* req = comm.get_request();
   EXPECT_EQ(req->command_id, 1);
   EXPECT_EQ(req->subfunction_id, 2);
   EXPECT_EQ(req->data_length, 0);
   
-  EXPECT_EQ(proto.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
+  EXPECT_EQ(comm.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
 }
 
 //=============================================================================
-TEST(TestProtocol_V1_0, TestRx_ZeroLen_BytePerByte)
+TEST_F(TestRxParsing, TestRx_ZeroLen_BytePerByte)
 {
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
-
   uint8_t data[8] = {1,2,0,0};
   scrutiny_test::add_crc(data, 4);
 
   for (unsigned int i=0; i<sizeof(data); i++)
   {
-    proto.process_data(&data[i], 1);
+    comm.process_data(&data[i], 1);
   }
 
-  ASSERT_TRUE(proto.request_received());
-  scrutiny::Protocol::Request* req = proto.get_request();
+  ASSERT_TRUE(comm.request_received());
+  scrutiny::Protocol::Request* req = comm.get_request();
   EXPECT_EQ(req->command_id, 1);
   EXPECT_EQ(req->subfunction_id, 2);
   EXPECT_EQ(req->data_length, 0);
 
-  EXPECT_EQ(proto.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
+  EXPECT_EQ(comm.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
 }
 
 //=============================================================================
-TEST(TestProtocol_V1_0, TestRx_NonZeroLen_AllInOne)
+TEST_F(TestRxParsing, TestRx_NonZeroLen_AllInOne)
 {
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
-
   uint8_t data[11] = {1,2,0,3, 0x11, 0x22, 0x33};
   scrutiny_test::add_crc(data, 7);
-  proto.process_data(data, sizeof(data));
+  comm.process_data(data, sizeof(data));
 
-  ASSERT_TRUE(proto.request_received());
-  scrutiny::Protocol::Request* req = proto.get_request();
+  ASSERT_TRUE(comm.request_received());
+  scrutiny::Protocol::Request* req = comm.get_request();
   EXPECT_EQ(req->command_id, 1);
   EXPECT_EQ(req->subfunction_id, 2);
   EXPECT_EQ(req->data_length, 3);
@@ -67,26 +69,22 @@ TEST(TestProtocol_V1_0, TestRx_NonZeroLen_AllInOne)
   EXPECT_EQ(req->data[1], 0x22);
   EXPECT_EQ(req->data[2], 0x33);
   
-  EXPECT_EQ(proto.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
+  EXPECT_EQ(comm.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
 }
 
 //=============================================================================
-TEST(TestProtocol_V1_0, TestRx_NonZeroLen_BytePerByte)
+TEST_F(TestRxParsing, TestRx_NonZeroLen_BytePerByte)
 {
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
-
   uint8_t data[11] = {1,2,0,3, 0x11, 0x22, 0x33};
   scrutiny_test::add_crc(data, 7);
 
   for (unsigned int i=0; i<sizeof(data); i++)
   {
-    proto.process_data(&data[i], 1);
+    comm.process_data(&data[i], 1);
   }
 
-  ASSERT_TRUE(proto.request_received());
-  scrutiny::Protocol::Request* req = proto.get_request();
+  ASSERT_TRUE(comm.request_received());
+  scrutiny::Protocol::Request* req = comm.get_request();
   EXPECT_EQ(req->command_id, 1);
   EXPECT_EQ(req->subfunction_id, 2);
   EXPECT_EQ(req->data_length, 3);
@@ -94,60 +92,49 @@ TEST(TestProtocol_V1_0, TestRx_NonZeroLen_BytePerByte)
   EXPECT_EQ(req->data[1], 0x22);
   EXPECT_EQ(req->data[2], 0x33);
   
-  EXPECT_EQ(proto.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
+  EXPECT_EQ(comm.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorNone);
 }
 
 //=============================================================================
-TEST(TestProtocol_V1_0, TestRx_Overflow)
+TEST_F(TestRxParsing, TestRx_Overflow)
 {
   ASSERT_LT(SCRUTINY_BUFFER_SIZE, 0xFFFF-1);  // Lengths are 16bits maximum by protocol definition
 
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
   uint16_t datalen = SCRUTINY_BUFFER_SIZE + 1;
 
   uint8_t data[SCRUTINY_BUFFER_SIZE+8] = {1,2, static_cast<uint8_t>((datalen >> 8) & 0xFF) , static_cast<uint8_t>(datalen & 0xFF)};
   scrutiny_test::add_crc(data, SCRUTINY_BUFFER_SIZE+4);
 
-  proto.process_data(data, sizeof(data));
+  comm.process_data(data, sizeof(data));
 
-  ASSERT_FALSE(proto.request_received());
-  EXPECT_EQ(proto.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorOverflow);
+  ASSERT_FALSE(comm.request_received());
+  EXPECT_EQ(comm.get_rx_error(), scrutiny::Protocol::CommHandler::eRxErrorOverflow);
 }
 
 //=============================================================================
-TEST(TestProtocol_V1_0, TestRx_Timeout)
+TEST_F(TestRxParsing, TestRx_Timeout)
 {
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
-
   uint8_t data[11] = {1,2,0,3, 0x11, 0x22, 0x33};
   scrutiny_test::add_crc(data, 7);
 
   for (uint8_t i=1; i < sizeof(data)-1; i++)
   {
-    proto.process_data(&data[0], i );
-    ASSERT_FALSE(proto.request_received());
+    comm.process_data(&data[0], i );
+    ASSERT_FALSE(comm.request_received());
     tb.step(SCRUTINY_COMM_TIMEOUT_US);
-    proto.process_data(&data[i], sizeof(data)-1 );
-    ASSERT_FALSE(proto.request_received());
-    proto.reset();
+    comm.process_data(&data[i], sizeof(data)-1 );
+    ASSERT_FALSE(comm.request_received());
+    comm.reset();
   }
 }
 
 //=============================================================================
-TEST(TestProtocol_V1_0, TestRx_BadCRC)
+TEST_F(TestRxParsing, TestRx_BadCRC)
 {
-  scrutiny::Timebase tb;
-  scrutiny::Protocol::CommHandler proto;
-  proto.init(1,0, &tb);
-
   uint8_t data[11] = {1,2,0,3, 0x11, 0x22, 0x33};
   scrutiny_test::add_crc(data, 7);
   data[10] = ~data[10]; // Force bad CRC
-  proto.process_data(data, sizeof(data));
+  comm.process_data(data, sizeof(data));
 
-  ASSERT_FALSE(proto.request_received());
+  ASSERT_FALSE(comm.request_received());
 }
