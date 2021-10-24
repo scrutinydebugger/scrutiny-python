31ae247ca3b25a2117327c3a55a59705d278adc5
Pier-Yves Lessard <py.lessard@gmail.com>
2021-10-24 10:16:56 -0400

    Removed personal script

--- python/scrutiny/cli/loadTestApp.bat
+++
@@ -1,3 +0,0 @@
-copy \\wsl$\Debian\home\py\scrutiny\firmware\build\testapp\testapp .\testapp
-copy \\wsl$\Debian\home\py\scrutiny\firmware\build\testapp\testapp .\test\files\testappDebianLEx64_gcc8_3_0
-copy \\wsl$\Debian\home\py\scrutiny\firmware\testapp_memdump .\test\files\testappDebianLEx64_gcc8_3_0.memdump
