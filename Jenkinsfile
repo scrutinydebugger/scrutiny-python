fc4dfef5e1a4a64febb4c6cda483e7c34338548b
Pier-Yves Lessard <py.lessard@gmail.com>
2022-06-29 15:08:04 -0400

    jenkins test

--- Jenkinsfile
+++ Jenkinsfile
@@ -14,7 +14,9 @@ pipeline {
                 stage ('Test Python 3.10') {
                     steps {
                         sh ''' 
+                        echo $(which python3)
                         alias python3=python3.10
+                        echo $(which python3)
                         alias pip3=pip3.10
                         scripts/check_python_version.sh 3.10 && scripts/runtests.sh
                         '''
--- scripts/check_python_version.sh
+++ scripts/check_python_version.sh
@@ -2,17 +2,17 @@
 
 shopt -s nocasematch
 
-VERSION=$1
+REQUIRED_VERSION=$1
 ACTUAL_PYTHON_VERSION=$(python3 --version)
 ACTUAL_PIP_VERSION=$(pip3 --version)
 
-if [[ $ACTUAL_PYTHON_VERSION != *"python ${VERSION}"* ]]; then
+if [[ $ACTUAL_PYTHON_VERSION != *"python ${REQUIRED_VERSION}"* ]]; then
     
     echo "ERROR - Reported python3 version is ${ACTUAL_PYTHON_VERSION}"
     exit 1
 fi
 
-if [[ $ACTUAL_PIP_VERSION != *"python ${VERSION}"* ]]; then 
+if [[ $ACTUAL_PIP_VERSION != *"python ${REQUIRED_VERSION}"* ]]; then 
     echo "ERROR - Reported pip3 version is ${ACTUAL_PIP_VERSION}"
     exit 1
 fi
