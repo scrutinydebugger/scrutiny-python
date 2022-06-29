c87e716c7154cdef1534189f787bdddd0e3e6729
Pier-Yves Lessard <py.lessard@gmail.com>
2022-06-29 15:57:10 -0400

    jenkins test

--- scripts/check_python_version.sh
+++ scripts/check_python_version.sh
@@ -8,12 +8,12 @@ ACTUAL_PIP_VERSION=$(pip3 --version)
 
 if [[ $ACTUAL_PYTHON_VERSION != *"python ${REQUIRED_VERSION}"* ]]; then
     
-    echo "ERROR - Reported python3 version is ${ACTUAL_PYTHON_VERSION}"
+    echo "ERROR - Reported python3 version is ${ACTUAL_PYTHON_VERSION}. Expecting Python ${REQUIRED_VERSION}"
     exit 1
 fi
 
 if [[ $ACTUAL_PIP_VERSION != *"python ${REQUIRED_VERSION}"* ]]; then 
-    echo "ERROR - Reported pip3 version is ${ACTUAL_PIP_VERSION}"
+    echo "ERROR - Reported pip3 version is ${ACTUAL_PIP_VERSION}. Expecting pip for Python ${REQUIRED_VERSION}"
     exit 1
 fi
 
