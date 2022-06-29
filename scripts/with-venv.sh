0ac3486a94416d6019db445631b5fd2590c6af28
Pier-Yves Lessard <py.lessard@gmail.com>
2022-06-28 23:49:09 -0400

    changed scripts path

--- scripts/with-venv.sh
+++ scripts/with-venv.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. >/dev/null 2>&1 && pwd -P )"
-source "$PROJECT_ROOT/python/scripts/activate-venv.sh"
+source "$PROJECT_ROOT/scripts/activate-venv.sh"
 
 exec "$@"
