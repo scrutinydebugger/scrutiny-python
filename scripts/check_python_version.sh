841006885262d85aac4de3734f4f2b1cf8d77646
Pier-Yves Lessard <py.lessard@gmail.com>
2022-06-29 13:30:11 -0400

    more verbose failure

--- scripts/check_python_version.sh
+++ scripts/check_python_version.sh
@@ -4,10 +4,14 @@ shopt -s nocasematch
 
 VERSION=$1
 if [[ $(python3 --version) != *"python ${VERSION}"* ]]; then
+    echo "Reported python3 version is "
+    python3 --version
     exit 1
 fi
 
 if [[ $(pip3 --version) != *"python ${VERSION}"* ]]; then 
+    echo "Reported pip3 version is "
+    pip3 --version
     exit 1
 fi
 
