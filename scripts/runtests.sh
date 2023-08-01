077f15169afc919ad15cd8b2076d1ec88c6eff95
Pier-Yves Lessard <py.lessard@gmail.com>
2023-07-31 23:08:37 -0400

    Do not remove html report on CI

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -9,13 +9,4 @@ COV_DATAFILE=".coverage_${SCRUTINY_COVERAGE_SUFFIX}"
 python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest
 python3 -m mypy scrutiny
 python3 -m coverage report --data-file ${COV_DATAFILE}
-
-MAKE_HTML=1
-if ! [[ -z "${BUILD_CONTEXT+x}" ]]; then
-    if [[ "$BUILD_CONTEXT" == "ci" ]]; then
-        MAKE_HTML=0 # No HTML report on CI
-    fi
-fi
-if [ $MAKE_HTML ]; then
-    python3 -m coverage html --data-file ${COV_DATAFILE} -d $HTML_COVDIR
-fi
+python3 -m coverage html --data-file ${COV_DATAFILE} -d $HTML_COVDIR
