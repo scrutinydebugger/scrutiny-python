cbed411943d966a462f1da6a26f0f8280a3d4581
Pier-Yves Lessard <py.lessard@gmail.com>
2023-07-31 23:00:51 -0400

    Working solution

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,8 +2,20 @@
 set -euxo pipefail
 
 SCRUTINY_COVERAGE_SUFFIX="${SCRUTINY_COVERAGE_SUFFIX:-dev}"
-COV_DATAFILE=".coverage${SCRUTINY_COVERAGE_SUFFIX}"
+
+HTML_COVDIR="htmlcov_${SCRUTINY_COVERAGE_SUFFIX}"
+COV_DATAFILE=".coverage_${SCRUTINY_COVERAGE_SUFFIX}"
 
 python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest
 python3 -m mypy scrutiny
-python3 -m coverage report
+python3 -m coverage report --data-file ${COV_DATAFILE}
+
+MAKE_HTML=1
+if ! [[ -z "${BUILD_CONTEXT+x}" ]]; then
+    if [[ "$BUILD_CONTEXT" == "ci" ]]; then
+        MAKE_HTML=0 # No HTML report on CI
+    fi
+fi
+if [ $MAKE_HTML ]; then
+    python3 -m coverage html --data-file ${COV_DATAFILE} -d $HTML_COVDIR
+fi
