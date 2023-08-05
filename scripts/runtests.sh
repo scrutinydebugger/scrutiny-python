16bbc2fbb97b964f99e76d2ab2e594f3ff41c233
Pier-Yves Lessard <py.lessard@gmail.com>
2023-08-05 18:56:20 -0400

    Removed debug logging + more rigourous agent check

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -6,11 +6,15 @@ SCRUTINY_COVERAGE_SUFFIX="${SCRUTINY_COVERAGE_SUFFIX:-dev}"
 HTML_COVDIR="htmlcov_${SCRUTINY_COVERAGE_SUFFIX}"
 COV_DATAFILE=".coverage_${SCRUTINY_COVERAGE_SUFFIX}"
 
-if ! [[ -z "${NODE_NAME+x}" ]]; then
-    echo "Running tests on agent: ${NODE_NAME}"
+if ! [[ -z "${BUILD_CONTEXT+x}" ]]; then
+    if [[ "$BUILD_CONTEXT" == "ci" ]]; then
+        if ! [[ -z "${NODE_NAME+x}" ]]; then
+            echo "Running tests on agent: ${NODE_NAME}"
+        fi
+    fi
 fi
 
-python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest --loglevel debug
+python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest
 python3 -m mypy scrutiny
 python3 -m coverage report --data-file ${COV_DATAFILE}
 python3 -m coverage html --data-file ${COV_DATAFILE} -d $HTML_COVDIR
