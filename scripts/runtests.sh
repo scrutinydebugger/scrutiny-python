d1ebaf6430c5fc57e8b9cd8961f5e4cdfaed1ce2
Pier-Yves Lessard <py.lessard@gmail.com>
2023-08-05 13:24:38 -0400

    Try add print agent name

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -6,6 +6,10 @@ SCRUTINY_COVERAGE_SUFFIX="${SCRUTINY_COVERAGE_SUFFIX:-dev}"
 HTML_COVDIR="htmlcov_${SCRUTINY_COVERAGE_SUFFIX}"
 COV_DATAFILE=".coverage_${SCRUTINY_COVERAGE_SUFFIX}"
 
+if ! [[ -z "${NODE_NAME+x}" ]]; then
+    echo "Node Name = ${NODE_NAME}"
+fi
+
 python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest --loglevel debug
 python3 -m mypy scrutiny
 python3 -m coverage report --data-file ${COV_DATAFILE}
