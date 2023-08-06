98380c1aa6be46096de9b4ce3f9db795eda53e46
Pier-Yves Lessard <py.lessard@gmail.com>
2023-08-05 22:58:04 -0400

    Removed debug log + removed useless verbosity in runstest.sh

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -euxo pipefail
+set -uxo pipefail
 
 SCRUTINY_COVERAGE_SUFFIX="${SCRUTINY_COVERAGE_SUFFIX:-dev}"
-
 HTML_COVDIR="htmlcov_${SCRUTINY_COVERAGE_SUFFIX}"
 COV_DATAFILE=".coverage_${SCRUTINY_COVERAGE_SUFFIX}"
 
@@ -14,7 +13,9 @@ if ! [[ -z "${BUILD_CONTEXT+x}" ]]; then
     fi
 fi
 
-python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest --loglevel debug
+set -e 
+
+python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest
 python3 -m mypy scrutiny
 python3 -m coverage report --data-file ${COV_DATAFILE}
 python3 -m coverage html --data-file ${COV_DATAFILE} -d $HTML_COVDIR
