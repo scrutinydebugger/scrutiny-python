3885d3ac17126c4274dacdb0b634e454095311b4
Pier-Yves Lessard <py.lessard@gmail.com>
2024-01-14 08:22:34 -0500

    Enforce --strict

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -16,7 +16,7 @@ fi
 set -x 
 
 python3 -m coverage run --data-file ${COV_DATAFILE} -m scrutiny runtest
-python3 -m mypy scrutiny
+python3 -m mypy scrutiny --strict
 python3 -m coverage report --data-file ${COV_DATAFILE}
 python3 -m coverage html --data-file ${COV_DATAFILE} -d $HTML_COVDIR
   
