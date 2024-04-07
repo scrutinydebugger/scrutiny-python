061d6ac43c8fe16d9e3427ebaea701d018a7afa3
Pier-Yves Lessard <py.lessard@gmail.com>
2024-04-06 22:52:47 -0400

    fixed mypy

--- scrutiny/sdk/listeners/buffered_reader_listener.py
+++ scrutiny/sdk/listeners/buffered_reader_listener.py
@@ -25,6 +25,6 @@ class BufferedReaderListener(BaseListener):
         for update in updates:
             self._queue.put(update)
 
-    def get_queue(self) -> SimpleQueue:
+    def get_queue(self) -> "SimpleQueue[ValueUpdate]":
         """Return the queue used for storage"""
         return self._queue
