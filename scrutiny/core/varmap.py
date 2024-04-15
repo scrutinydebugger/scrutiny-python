b27aa8de897c5040f045e7c741f3abfb231d7981
Pier-Yves Lessard <py.lessard@gmail.com>
2024-04-14 21:01:40 -0400

    Added indent option

--- scrutiny/core/varmap.py
+++ scrutiny/core/varmap.py
@@ -116,11 +116,11 @@ class VarMap:
     def get_endianness(self) -> Endianness:
         return self.endianness
 
-    def write(self, filename: str) -> None:
+    def write(self, filename: str, indent:int=4) -> None:
         with open(filename, 'w') as f:
-            f.write(self.get_json())
+            f.write(self.get_json(indent))
 
-    def get_json(self) -> str:
+    def get_json(self, indent:int=4) -> str:
         if self.endianness == Endianness.Little:
             endianness_str = 'little'
         elif self.endianness == Endianness.Big:
@@ -134,7 +134,7 @@ class VarMap:
             'variables': self.variables,
             'enums': self.enums,
         }
-        return json.dumps(content, indent=4)
+        return json.dumps(content, indent=indent)
 
     def validate(self) -> None:
         pass
