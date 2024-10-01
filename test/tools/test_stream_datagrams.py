915f5b027103f96e887300dab05fb657a08c5673
Pier-Yves Lessard <py.lessard@gmail.com>
2024-09-30 22:53:36 -0400

    stream/datagram converter encode features in flags and enable compression

--- test/tools/test_stream_datagrams.py
+++ test/tools/test_stream_datagrams.py
@@ -6,101 +6,134 @@
 #
 #   Copyright (c) 2021 Scrutiny Debugger
 
-from scrutiny.tools.stream_datagrams import StreamMaker, StreamParser
+from scrutiny.tools.stream_datagrams import StreamMaker, StreamParser, COMPRESSION_LEVEL
 from test import logger
 from test import ScrutinyUnitTest
 from hashlib import md5
 import time
+import zlib
 
 DEFAULT_MTU = 1024*1024
 
 class TestStreamDatagrams(ScrutinyUnitTest):
     def test_stream_maker(self):
         data = bytes( [1,2,3,4] )
-        payload = bytes(StreamMaker(mtu=DEFAULT_MTU, use_hash=True).encode(data))
-        self.assertEqual(b"<SCRUTINY size=4>\x01\x02\x03\x04" + md5(data).digest(), payload)
+        payload = bytes(StreamMaker(mtu=DEFAULT_MTU, use_hash=True, compress=False).encode(data))
+        self.assertEqual(b"<SCRUTINY size=4 flags=h>\x01\x02\x03\x04" + md5(data).digest(), payload)
 
         data = bytes( [1,2,3,4] )
-        payload = bytes(StreamMaker(mtu=DEFAULT_MTU, use_hash=False).encode(data))
-        self.assertEqual(b"<SCRUTINY size=4>\x01\x02\x03\x04", payload)
+        payload = bytes(StreamMaker(mtu=DEFAULT_MTU, use_hash=False, compress =False).encode(data))
+        self.assertEqual(b"<SCRUTINY size=4 flags=>\x01\x02\x03\x04", payload)
+
+        compressed_data = zlib.compress(data, level=COMPRESSION_LEVEL)
+        compressed_len_str = f'{len(compressed_data):x}'.encode("utf8")
+        payload = bytes(StreamMaker(mtu=DEFAULT_MTU, use_hash=True, compress=True).encode(data))
+        self.assertEqual(b"<SCRUTINY size=" + compressed_len_str + b" flags=ch>" + compressed_data + md5(compressed_data).digest(), payload)
+
+        payload = bytes(StreamMaker(mtu=DEFAULT_MTU, use_hash=False, compress =True).encode(data))
+        self.assertEqual(b"<SCRUTINY size=" + compressed_len_str + b" flags=c>" + compressed_data , payload)
 
         with self.assertRaises(Exception):
-            StreamMaker(mtu=4, use_hash=False).encode("abcde".encode('ascii'))
-        StreamMaker(mtu=4, use_hash=False).encode("abcd".encode('ascii'))   # no exception
+            StreamMaker(mtu=4, use_hash=False, compress=False).encode("abcde".encode('ascii'))
+        StreamMaker(mtu=4, use_hash=False, compress=False).encode("abcd".encode('ascii'))   # no exception
 
     def test_stream_parser_simple_read_hash(self):
         data = bytes( [1,2,3,4] )
-        payload = b"<SCRUTINY size=4>" + data + md5(data).digest()
+        payload = b"<SCRUTINY size=4 flags=h>" + data + md5(data).digest()
 
-        parser = StreamParser(mtu=DEFAULT_MTU, use_hash=True)
+        parser = StreamParser(mtu=DEFAULT_MTU)
         parser.parse(payload)
         self.assertFalse(parser.queue().empty())
         self.assertEqual(parser.queue().get(), data)      
 
     def test_stream_parser_simple_read_nohash(self):
         data = bytes( [1,2,3,4] )
-        payload = b"<SCRUTINY size=4>" + data
+        payload = b"<SCRUTINY size=4 flags=>" + data
 
-        parser = StreamParser(mtu=DEFAULT_MTU, use_hash=False)
+        parser = StreamParser(mtu=DEFAULT_MTU)
         parser.parse(payload)
         self.assertFalse(parser.queue().empty())
-        self.assertEqual(parser.queue().get(), data)        
+        self.assertEqual(parser.queue().get(), data)      
+
+    def test_stream_parser_simple_read_hash_compress(self):
+        data = bytes( [1,2,3,4] )
+        compressed_data = zlib.compress(data, level=COMPRESSION_LEVEL)
+        compressed_len_str = f'{len(compressed_data):x}'.encode('utf8')
+        payload = b"<SCRUTINY size=" + compressed_len_str + b" flags=ch>" + compressed_data + md5(compressed_data).digest()
+
+        parser = StreamParser(mtu=DEFAULT_MTU)
+        parser.parse(payload)
+        self.assertFalse(parser.queue().empty())
+        self.assertEqual(parser.queue().get(), data)  
+
+    def test_stream_parser_simple_read_nohash_compress(self):
+        data = bytes( [1,2,3,4] )
+        compressed_data = zlib.compress(data, level=COMPRESSION_LEVEL)
+        compressed_len_str = f'{len(compressed_data):x}'.encode('utf8')
+        payload = b"<SCRUTINY size=" + compressed_len_str + b" flags=c>" + compressed_data
+
+        parser = StreamParser(mtu=DEFAULT_MTU)
+        parser.parse(payload)
+        self.assertFalse(parser.queue().empty())
+        self.assertEqual(parser.queue().get(), data)     
     
     def test_stream_parser_bad_size(self):
         data = bytes( [1,2,3,4] )
         for i in range (10):
             if i != len(data):
-                payload = b"<SCRUTINY size=" + str(i).encode('utf8') + b">" + data + md5(data).digest()
-                parser = StreamParser(mtu=DEFAULT_MTU, use_hash=True)
+                payload = b"<SCRUTINY size=" + str(i).encode('utf8') + b" flags=h>" + data + md5(data).digest()
+                parser = StreamParser(mtu=DEFAULT_MTU)
                 parser.parse(payload)
                 self.assertTrue(parser.queue().empty())
         
     def test_stream_parser_multi_datagrams_single_chunk(self):
-        for use_hash in True, False:
-            datas = [
-                bytes([0x01,0x02,0x03,0x04]),
-                bytes([0x11,0x12,0x13,0x14, 0x15, 0x16]),
-                bytes([0x21,0x22,0x23]),
-            ]
+        for use_compression in True, False:
+            for use_hash in True, False:
+                datas = [
+                    bytes([0x01,0x02,0x03,0x04]),
+                    bytes([0x11,0x12,0x13,0x14, 0x15, 0x16]),
+                    bytes([0x21,0x22,0x23]),
+                ]
 
-            payload = bytearray()
-            payload.extend("garbage!".encode('ascii'))
-            for data in datas:
-                payload.extend(StreamMaker(mtu=DEFAULT_MTU, use_hash=use_hash).encode(data))
-                payload.extend("garbage!...".encode('ascii'))
-            
-            parser = StreamParser(mtu=DEFAULT_MTU, use_hash=use_hash)
-            parser.parse(payload)
-            q = parser.queue()
-            for i in range(len(datas)):
-                self.assertFalse(q.empty(), f"i={i}")
-                self.assertEqual(q.get(), datas[i], f"i={i}")
-            self.assertTrue(q.empty())
+                payload = bytearray()
+                payload.extend("garbage!".encode('ascii'))
+                for data in datas:
+                    payload.extend(StreamMaker(mtu=DEFAULT_MTU, use_hash=use_hash, compress=use_compression).encode(data))
+                    payload.extend("garbage!...".encode('ascii'))
+                
+                parser = StreamParser(mtu=DEFAULT_MTU)
+                parser.parse(payload)
+                q = parser.queue()
+                for i in range(len(datas)):
+                    self.assertFalse(q.empty(), f"i={i}")
+                    self.assertEqual(q.get(), datas[i], f"i={i}")
+                self.assertTrue(q.empty())
 
 
 
     def test_stream_parser_multi_datagrams_byte_per_bytes(self):
-        for use_hash in True, False:
-            datas = [
-                bytes([0x01,0x02,0x03,0x04]),
-                bytes([0x11,0x12,0x13,0x14, 0x15, 0x16]),
-                bytes([0x21,0x22,0x23]),
-            ]
+        for use_compression in True, False:
+            for use_hash in True, False:
+                datas = [
+                    bytes([0x01,0x02,0x03,0x04]),
+                    bytes([0x11,0x12,0x13,0x14, 0x15, 0x16]),
+                    bytes([0x21,0x22,0x23]),
+                ]
 
-            payload = bytearray()
-            payload.extend("garbage!".encode('ascii'))
-            for data in datas:
-                payload.extend(StreamMaker(mtu=DEFAULT_MTU, use_hash=use_hash).encode(data))
-                payload.extend("garbage!...".encode('ascii'))
-            
-            parser = StreamParser(mtu=DEFAULT_MTU, use_hash=use_hash)
-            for b in payload:
-                parser.parse(bytes([b]))
-            q = parser.queue()
-            for i in range(len(datas)):
-                self.assertFalse(q.empty(), f"i={i}")
-                self.assertEqual(q.get(), datas[i], f"i={i}")
-            self.assertTrue(q.empty())
+                payload = bytearray()
+                payload.extend("garbage!".encode('ascii'))
+                for data in datas:
+                    payload.extend(StreamMaker(mtu=DEFAULT_MTU, use_hash=use_hash, compress=use_compression).encode(data))
+                    payload.extend("garbage!...".encode('ascii'))
+                
+                parser = StreamParser(mtu=DEFAULT_MTU)
+                for b in payload:
+                    parser.parse(bytes([b]))
+                q = parser.queue()
+                for i in range(len(datas)):
+                    self.assertFalse(q.empty(), f"i={i}")
+                    self.assertEqual(q.get(), datas[i], f"i={i}")
+                self.assertTrue(q.empty())
 
     def test_stream_parser_timeout(self):
         for use_hash in False, :
@@ -120,7 +153,7 @@ class TestStreamDatagrams(ScrutinyUnitTest):
                     split = len(payload)-2
                 payload.extend("garbage!...".encode('ascii'))
             
-            parser = StreamParser(mtu=DEFAULT_MTU, interchunk_timeout=0.2, use_hash=use_hash)
+            parser = StreamParser(mtu=DEFAULT_MTU, interchunk_timeout=0.2)
             parser.parse(payload[0:split])
             time.sleep(0.3)
             parser.parse(payload[split:])
@@ -147,14 +180,14 @@ class TestStreamDatagrams(ScrutinyUnitTest):
                 bytes([0x21,0x22,0x23]),
             ]
 
-            maker = StreamMaker(mtu=6, use_hash=use_hash)   # Stream Maker makes bigger payload than the aprser
+            maker = StreamMaker(mtu=6, use_hash=use_hash, compress=False)   # Stream Maker makes bigger payload than the parser
             payload = bytearray()
             payload.extend("garbage!".encode('ascii'))
             for data in datas:
                 payload.extend(maker.encode(data))
                 payload.extend("garbage!...".encode('ascii'))
             
-            parser = StreamParser(mtu=4, interchunk_timeout=0.2, use_hash=use_hash)
+            parser = StreamParser(mtu=4, interchunk_timeout=0.2)
             parser.parse(payload)
             
             q = parser.queue()
