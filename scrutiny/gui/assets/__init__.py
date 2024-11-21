959430672984221b2a241c100c0d944ea99e1a50
Pier-Yves Lessard <py.lessard@gmail.com>
2024-11-20 23:03:29 -0500

    Architectural work. + lazy laoding in varlist widget

--- scrutiny/gui/assets/__init__.py
+++ scrutiny/gui/assets/__init__.py
@@ -1,9 +1,19 @@
+__all__ = [
+    'get',
+    'load_bin',
+    'load_text',
+    'logo_icon',
+    'load_pixmap',
+    'load_icon',
+    'Icons'
+]
+
 import os
 from scrutiny.gui.core.exceptions import GuiError
 from pathlib import Path
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QPixmap, QIcon
 
-from typing import List, Union
+from typing import List, Union, Dict
 
 ASSET_PATH = os.path.dirname(__file__)
 
@@ -29,4 +39,21 @@ def logo_icon() -> Path:
     return get('scrutiny-logo-square-64x64.png')
 
 def load_pixmap(name:str) -> QPixmap:
-    return QPixmap(str(get(name)))
+    if name not in pixmap_cache:
+        pixmap_cache[name] = QPixmap(str(get(name)))
+    return pixmap_cache[name]
+
+def load_icon(name:str) -> QPixmap:
+    if name not in icon_cache:
+        icon_cache[name] = QIcon(str(get(name)))
+    return icon_cache[name]
+
+
+icon_cache: Dict[str, QIcon] = {}
+pixmap_cache: Dict[str, QIcon] = {}
+
+class Icons:
+    TreeFolder = "folder-16x16.png"
+    TreeVar = "var-16x16.png"
+    TreeRpv = "rpv-16x16.png"
+    TreeAlias = "alias-16x16.png"
--- scrutiny/gui/dashboard_components/varlist/varlist_component.py
+++ scrutiny/gui/dashboard_components/varlist/varlist_component.py
@@ -8,18 +8,21 @@
 #   Copyright (c) 2021 Scrutiny Debugger
 
 from scrutiny.gui.dashboard_components.base_component import ScrutinyGUIBaseComponent
-from typing import Dict, Any, List
+from typing import Dict, Any, List, cast, Optional
 
 from PySide6.QtWidgets import QVBoxLayout
 from PySide6.QtGui import QStandardItemModel, QStandardItem, QIcon
+from PySide6.QtCore import QModelIndex, Qt
 
+from scrutiny.gui.core.watchable_index import ParsedFullyQualifiedName
 from scrutiny.gui import assets
 from scrutiny.gui.widgets.multiselect_treeview import MultiSelectTreeView
 from scrutiny.gui.core.watchable_index import WatchableIndexNodeContent
 from scrutiny.sdk import WatchableType, WatchableConfiguration
+from scrutiny.gui.dashboard_components.common.tree_items import FolderStandardItem, WatchableStandardItem, StandardItemWithFQN
 
+LOADED_ROLE = Qt.ItemDataRole.UserRole + 1
 
-        
 class VarListComponent(ScrutinyGUIBaseComponent):
     instance_name : str
 
@@ -42,30 +45,25 @@ class VarListComponent(ScrutinyGUIBaseComponent):
     def __init__(self, *args:Any, **kwargs:Any) -> None:
         super().__init__(*args, **kwargs)
 
-        self.FOLDER_ICON = QIcon(assets.load_pixmap("folder-16x16.png"))
-        self.VAR_ICON = QIcon(assets.load_pixmap("var-16x16.png"))
-        self.RPV_ICON = QIcon(assets.load_pixmap("rpv-16x16.png"))
-        self.ALIAS_ICON = QIcon(assets.load_pixmap("alias-16x16.png"))
+        self.FOLDER_ICON =assets.load_icon(assets.Icons.TreeFolder)
+        self.VAR_ICON =assets.load_icon(assets.Icons.TreeVar)
+        self.RPV_ICON =assets.load_icon(assets.Icons.TreeRpv)
+        self.ALIAS_ICON =assets.load_icon(assets.Icons.TreeAlias)
 
-    def get_watchable_icon(self, wt:WatchableType) -> QIcon:
-        if wt == WatchableType.Variable:
-            return self.VAR_ICON
-        if wt == WatchableType.Alias:
-            return self.ALIAS_ICON
-        if wt == WatchableType.RuntimePublishedValue:
-            return self.RPV_ICON
-        raise NotImplementedError(f"Unsupported icon for {wt}")
+    def make_fqn(self, watchable_type:WatchableType, path:str) -> str:
+        return self.server_manager.index.make_fqn(watchable_type, path)
+    
+    def parse_fqn(self, fqn:str) -> ParsedFullyQualifiedName:
+        return self.server_manager.index.parse_fqn(fqn)
 
     def make_folder_item(self, name:str, fqn:str) -> QStandardItem:
-        item =  QStandardItem(self.FOLDER_ICON, name)
-        item.setData(fqn)
+        item =  FolderStandardItem(name, fqn)
         item.setEditable(False)
         item.setDragEnabled(True)
         return item
 
     def make_watchable_item(self, name:str, watchable:WatchableConfiguration, fqn:str) -> QStandardItem:
-        item =  QStandardItem(self.get_watchable_icon(watchable.watchable_type), name)
-        item.setData(fqn)
+        item =  WatchableStandardItem(watchable.watchable_type, name, fqn)
         item.setEditable(False)
         item.setDragEnabled(True)
         return item
@@ -75,15 +73,16 @@ class VarListComponent(ScrutinyGUIBaseComponent):
 
         self._model = QStandardItemModel(0, 3, self)
         
-        self._var_folder = self.make_folder_item("Var", self.server_manager.index.make_fqn(WatchableType.Variable, '/'))
-        self._alias_folder = self.make_folder_item("Alias", self.server_manager.index.make_fqn(WatchableType.Alias, '/'))
-        self._rpv_folder = self.make_folder_item("RPV", self.server_manager.index.make_fqn(WatchableType.RuntimePublishedValue, '/'))
+        self._var_folder = self.make_folder_item("Var", self.make_fqn(WatchableType.Variable, '/'))
+        self._alias_folder = self.make_folder_item("Alias", self.make_fqn(WatchableType.Alias, '/'))
+        self._rpv_folder = self.make_folder_item("RPV", self.make_fqn(WatchableType.RuntimePublishedValue, '/'))
 
         self._model.appendRow(self._var_folder)
         self._model.appendRow(self._alias_folder)
         self._model.appendRow(self._rpv_folder)
+        self._model.setHorizontalHeaderLabels(['', 'Type', 'Enum'])
         self._treeview = MultiSelectTreeView()
-        self._treeview.setUniformRowHeights(True)   # Doc says it helps performance
+        self._treeview.setUniformRowHeights(True)   # Documentation says it helps performance
         self._treeview.setAnimated(False)
         
         self._treeview.setModel(self._model)
@@ -93,9 +92,22 @@ class VarListComponent(ScrutinyGUIBaseComponent):
         self._index_change_counters = self.server_manager.index.get_change_counters()
 
         self.server_manager.signals.index_changed.connect(self.index_changed_slot)
+        self._treeview.expanded.connect(self.node_expanded_slot)
+        
+    
+    def node_expanded_slot(self, index:QModelIndex):
+        #Lazy loading implementation
+        item = cast(StandardItemWithFQN, index.model().itemFromIndex(index))
+        for row in range(item.rowCount()):
+            child = cast(StandardItemWithFQN, item.child(row, 0))
+            if not child.data(LOADED_ROLE):
+                parsed_fqn = self.parse_fqn(child.fqn)
+                self.add_items_recursive(child, parsed_fqn.watchable_type, parsed_fqn.path, max_level=0)
+
     
     def index_changed_slot(self) -> None:
         index_change_counters = self.server_manager.index.get_change_counters()
+        # Identify all the types that changed since the last model update
         types_to_reload = []
         for wt, count in index_change_counters.items():
             if count != self._index_change_counters[wt]:
@@ -104,40 +116,56 @@ class VarListComponent(ScrutinyGUIBaseComponent):
         self._index_change_counters = index_change_counters
 
 
-
     def reload_model(self, watchable_types:List[WatchableType]) -> None:
         if WatchableType.RuntimePublishedValue in watchable_types:
             self._rpv_folder.removeRows(0, self._rpv_folder.rowCount())
-            self.add_items_recursive(self._rpv_folder, WatchableType.RuntimePublishedValue, '/')
+            # max_level=0 has we do lazy laoding
+            self.add_items_recursive(self._rpv_folder, WatchableType.RuntimePublishedValue, '/', max_level=0)
         
         if WatchableType.Alias in watchable_types:
             self._alias_folder.removeRows(0, self._alias_folder.rowCount())
-            self.add_items_recursive(self._alias_folder, WatchableType.Alias, '/')
+            # max_level=0 has we do lazy laoding
+            self.add_items_recursive(self._alias_folder, WatchableType.Alias, '/', max_level=0)
         
         if WatchableType.Variable in watchable_types:
             self._var_folder.removeRows(0, self._var_folder.rowCount())
-            self.add_items_recursive(self._var_folder, WatchableType.Variable, '/')
+            # max_level=0 has we do lazy laoding
+            self.add_items_recursive(self._var_folder, WatchableType.Variable, '/', max_level=0)
 
+    def make_watchable_row(self, item:QStandardItem, watchable_config:WatchableConfiguration) -> List[QStandardItem]:
+        typecol = QStandardItem(watchable_config.datatype.name)
+        typecol.setEditable(False)
+        if watchable_config.enum is not None:
+            enumcol = QStandardItem(watchable_config.enum.name)
+            enumcol.setEditable(False)
+            return (item, typecol, enumcol)
+        else:
+            return (item, typecol)
     
-    def add_items_recursive(self, parent:QStandardItem, watchable_type:WatchableType, path:str) -> None:
-        
+    def add_items_recursive(self, parent:QStandardItem, watchable_type:WatchableType, path:str, max_level:Optional[int]=None, level:int=0) -> None:
+        parent.setData(True, LOADED_ROLE)
         content = self.server_manager.index.read(watchable_type, path)
+        if path.endswith('/'):
+            path=path[:-1]
 
         if isinstance(content, WatchableIndexNodeContent):
             for name in content.subtree:
-                row = self.make_folder_item(name, path)
+                subtree_path = f'{path}/{name}'
+                row = self.make_folder_item(name, self.make_fqn(watchable_type, subtree_path))
                 parent.appendRow(row)
-                self.add_items_recursive(row, watchable_type, path + '/' + name)
+                if max_level is None or level < max_level:
+                    self.add_items_recursive(row, watchable_type, subtree_path, max_level=max_level, level=level+1)
             
             for name, watchable_config in content.watchables.items():
-                item = self.make_watchable_item(name, watchable_config, self.server_manager.index.make_fqn(watchable_type, path))
-                parent.appendRow([item, QStandardItem(watchable_config.datatype.name)])
+                wathcable_path = f'{path}/{name}'
+                item = self.make_watchable_item(name, watchable_config, self.make_fqn(watchable_type, wathcable_path))
+                parent.appendRow(self.make_watchable_row(item, watchable_config))
 
     def teardown(self) -> None:
         pass
 
     def get_state(self) -> Dict[Any, Any]:
-        raise NotImplementedError()
+        return {}
 
     def load_state(self, state: Dict[Any, Any]) -> None:
-        raise NotImplementedError()
+        pass
