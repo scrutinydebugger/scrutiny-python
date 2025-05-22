c6a0e3764ce9a02634598c210a159fad13442fc2
Pier-Yves Lessard <py.lessard@gmail.com>
2025-05-22 12:36:54 -0400

    Missing module + bump version to 0.4.1

--- scrutiny/__init__.py
+++ scrutiny/__init__.py
@@ -1,6 +1,6 @@
 __name__ = 'scrutiny'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __author__ = 'Pier-Yves Lessard'
-__credits__ = ['Pier-Yves Lessard', 'Frédéric Morin']
+__credits__ = ['Pier-Yves Lessard', 'Frédéric Morin', 'Joel Viau', 'Antoine Robidoux']
 __license__ = 'MIT'
 __status__ = 'Development'
