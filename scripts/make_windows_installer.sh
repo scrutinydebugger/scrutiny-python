b0515fce69575ba29070c469603d011afec86d3a
Pier-Yves Lessard <py.lessard@gmail.com>
2025-05-28 21:56:36 -0400

    Windows installer update

--- scripts/make_windows_installer.sh
+++ scripts/make_windows_installer.sh
@@ -6,6 +6,8 @@ source $(dirname ${BASH_SOURCE[0]})/common.sh
 NUITKA_OUTPUT=$(dir_with_default ${1:-""} "nuitka_build")
 PROJECT_ROOT="$(get_project_root)"
 SOURCE_DIR="${NUITKA_OUTPUT}/scrutiny.dist"
+OUTPUT_FOLDER=$(dir_with_default ${2:-""} "${NUITKA_OUTPUT}/installer")
+
 cd ${PROJECT_ROOT}
 
 # Check Folders exists
@@ -17,8 +19,18 @@ SCRUTINY_VERSION=$( ${SOURCE_DIR}/scrutiny.exe version --format short )
 
 info "Scrutiny version: $SCRUTINY_VERSION"
 assert_scrutiny_version_format "$SCRUTINY_VERSION"
+PKG_NAME="scrutinydebugger_v${SCRUTINY_VERSION}_setup"
 
 cp ${PROJECT_ROOT}/deploy/windows/scrutiny.ico ${SOURCE_DIR}/
 
-iscc ${PROJECT_ROOT}/deploy/windows/main.iss -DSOURCE_DIR="${SOURCE_DIR}" -DVERSION="${SCRUTINY_VERSION}"
-success "Installer generated"
+mkdir -p ${OUTPUT_FOLDER}
+iscc ${PROJECT_ROOT}/deploy/windows/main.iss    \
+    -DSOURCE_DIR="${SOURCE_DIR}"                \
+    -DVERSION="${SCRUTINY_VERSION}"             \
+    "-O${OUTPUT_FOLDER}"                        \
+    "-F${PKG_NAME}"  
+
+OUTFILE="${OUTPUT_FOLDER}/$PKG_NAME.exe"
+assert_file ${OUTFILE}
+
+success "Installer generated at: $OUTFILE"
