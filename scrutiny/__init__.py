fd513ab66017125485c963ed3728a0a2ed287e44
Pier-Yves Lessard <py.lessard@gmail.com>
2025-05-31 15:27:00 -0400

    Release v0.4.3

--- scrutiny/__init__.py
+++ scrutiny/__init__.py
@@ -11,7 +11,7 @@ __all__ = [
 
 
 __name__ = 'scrutiny'
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'Scrutiny Debugger'
 __credits__ = ['Pier-Yves Lessard', 'Frédéric Morin', 'Joel Viau', 'Antoine Robidoux']
 __license__ = 'MIT'
