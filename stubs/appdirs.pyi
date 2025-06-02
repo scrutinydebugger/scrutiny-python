d764b85688f4fb0890fe24a0a1dc075710f92b2f
Pier-Yves Lessard <py.lessard@gmail.com>
2025-06-02 17:02:23 -0400

    updated stub

--- stubs/appdirs.pyi
+++ stubs/appdirs.pyi
@@ -1,3 +1,11 @@
+#    appdirs.pyi
+#        A stub file for the appdirs module
+#
+#   - License : MIT - See LICENSE file.
+#   - Project :  Scrutiny Debugger (github.com/scrutinydebugger/scrutiny-main)
+#
+#   Copyright (c) 2025 Scrutiny Debugger
+
 from typing import Optional
 
 def user_data_dir(
@@ -48,3 +56,27 @@ def user_log_dir(
     version:Optional[str]=None, 
     opinion:bool=True
     ) -> str:...
+
+
+class AppDirs(object):
+    def __init__(self, 
+                 appname:Optional[str]=None, 
+                 appauthor:Optional[str]=None, 
+                 version:Optional[str]=None, 
+                 roaming:bool=False, 
+                 multipath:bool=False) -> None: ...
+
+    @property
+    def user_data_dir(self) -> str: ...
+    @property
+    def site_data_dir(self) -> str: ...
+    @property
+    def user_config_dir(self) -> str: ...
+    @property
+    def site_config_dir(self) -> str: ...
+    @property
+    def user_cache_dir(self) -> str: ...
+    @property
+    def user_state_dir(self) -> str: ...
+    @property
+    def user_log_dir(self) -> str: ...
